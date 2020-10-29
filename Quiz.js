c96705b9005e2280f69a9d09ef44583ec075fdb3
s-sutton3 <63067467+s-sutton3@users.noreply.github.com>
2020-10-29 13:53:06 -0600

    Quiz code
    Quiz code (not functional)

---
+++ Quiz.js
@@ -0,0 +1,43 @@
+document.getElementById("Mark").addEventListener("click", funkTion);
+
+// Make a quiz
+
+function funkTion() {
+
+// Var for math
+
+let i = 0;
+i++
+
+// If functions
+
+function qNo1() { 
+    if (a1 == "Reepicheep"){
+        i + 1
+    }else {
+       i = i
+    }
+}
+
+// Use if statements to make a passthrough for inputted text, should be relatively trivial compared to some of the assignments you've gotten.
+
+let question1 = document.getElementById("narniaQues").value.toLowerCase();
+let question2 = document.getElementById("thanosSnap").value.toLowerCase();
+let question3 = document.getElementById("hotFuzz").value.toLowerCase();
+let question4 = document.getElementById("lotrQues").value.toLowerCase();
+a1 = question1
+a2 = question2
+a3 = question3
+a4 = question4
+document.getElementById("Answer4").innerHTML
+
+// Proc
+
+
+
+
+// Disp res
+
+document.getElementById("Results").innerHTML + i
+
+}
